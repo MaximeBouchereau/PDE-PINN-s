97ac0f52d4d0ca1b58ab4e9a15fa13f6e69a8315
MaximeBouchereau <maximebouchereau@outlook.fr>
2026-02-20 16:37:03 +0100

    Add files via upload

---
+++ Control_ODE.py
@@ -0,0 +1,233 @@
+import warnings
+
+warnings.filterwarnings('ignore')
+warnings.warn('DelftStack')
+warnings.warn('Do not show this message')
+
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import copy
+
+import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from scipy.integrate import solve_ivp
+from scipy.optimize import fixed_point
+
+from itertools import product
+import statistics
+
+import sys
+import time
+import datetime
+from datetime import datetime as dtime
+
+# Python code to solve 1D Schrödinger equation with PINN's for Dirichlet boundary conditions
+
+L = np.pi    # Size of the (space) domain of resolution
+
+class NN(nn.Module):
+    """Class of Neural Networks used in this scipt"""
+
+    def __init__(self):
+        zeta , HL = 256 , 4
+        super().__init__()
+        self.Y = nn.ModuleList([nn.Linear(1, zeta), nn.Tanh()] + (HL - 1) * [nn.Linear(zeta, zeta), nn.Tanh()] + [nn.Linear(zeta, 2, bias=True)])
+        self.U = nn.ModuleList([nn.Linear(1, zeta), nn.Tanh()] + (HL - 1) * [nn.Linear(zeta, zeta), nn.Tanh()] + [nn.Linear(zeta, 1, bias=True)])
+
+    def y0(self):
+        """Initial condition."""
+        return torch.tensor([[1, 0]])
+
+    def y1(self):
+        """Final condition."""
+        return torch.tensor([[0, 2]])
+
+    def forward(self, t):
+        """Structured Neural Network.
+        Inputs:
+         - t: Tensor of shape (1,n) - time variable
+         """
+
+        t = t.float().T
+
+        y, y_0, u = t, torch.zeros_like(t), t
+
+        # Structure of the solution of the equation
+        for i, module in enumerate(self.Y):
+            y = module(y)
+            y_0 = module(y_0)
+
+        # Structure of the control of the equation
+        for i, module in enumerate(self.U):
+            u = module(u)
+
+        y, u = (y - y_0 + torch.kron(self.y0(), torch.ones_like(t))).T, u.T
+
+        return y, u
+
+class ML(NN):
+    """Training of the neural network for solving 1D heat equation"""
+
+    def Loss_autograd(self, t, T, model):
+        """Computes the Loss function associated with the PINN
+        Inputs:
+        - t: Tensor of shape (1,n): Inputs of Neural Network - time variable
+        - T: Float - Final time of ODE control
+        - model: Neural network which will be optimized for learning solution and control
+        Computes a predicted value uhat which is a tensor of shape (1,n) and returns the mean squared error between Yhat and Y.
+        Derivatives are approximated with autograd.
+        => Returns a tensor of shape (1,1)"""
+
+        t = torch.tensor(t, dtype=torch.float32)
+        t.requires_grad = True
+
+        ones = torch.ones_like(t)
+
+        y_hat = torch.cat((torch.zeros_like(t), torch.zeros_like(t)), dim=0)
+        y_hat.requires_grad = True
+
+        u_hat = torch.zeros_like(t)
+        u_hat.requires_grad = True
+
+        y_hat, u_hat = model(t)
+
+        y1_hat = y_hat[0, :]
+        y2_hat = y_hat[1, :]
+
+        y1_hat_t = torch.autograd.grad(y1_hat, t, grad_outputs=torch.ones_like(y1_hat), create_graph=True)[0]
+        y2_hat_t = torch.autograd.grad(y2_hat, t, grad_outputs=torch.ones_like(y2_hat), create_graph=True)[0]
+
+        loss_ODE = ((y1_hat_t + y2_hat - u_hat[0, :]) ** 2 + (y2_hat_t - y1_hat) ** 2).mean() # Loss associated to the PDE
+
+        y_hat_1 = model(torch.tensor([[T]]))[0]
+
+        loss_FC = ((y_hat_1 - self.y1().T) ** 2).mean() # Loss associated to final conditions (control)
+
+
+        return loss_ODE + loss_FC, loss_ODE, loss_FC
+
+    def Train_autograd(self, model , T = 1 , K = 1000 , BS = 64 ,  N_epochs = 100 , N_epochs_print = 10):
+        """Makes the training of the model to learn solution of PDE
+        Inputs:
+        - model: Neural network which will be optimized
+        - T: Float - Length of the time interval. Default: 1
+        - K: Int - Number of data for both train and test. Default: 1000
+        - BS: Int - Batch size. Default: 64
+        - N_epochs: Int - Number of epochs for gradient descent. Default: 100
+        - N_epochs_print: Int - Number of epochs between two prints of the Loss. Default: 10
+        Derivatives are approximated with autograd.
+        => Returns the lists Loss_train and Loss_test of the values of the Loss w.r.t. training and test,
+        and best_model, which is the best apporoximation of the desired model"""
+
+        start_time_train = time.time()
+
+        print(" ")
+        print(150 * "-")
+        print("Training...")
+        print(150 * "-")
+
+        #t_train , t_test = torch.linspace(0,T,K).reshape([1,K]) , T*torch.rand([1 , K])
+        #x_train , x_test = torch.linspace(-1,1,K).reshape([1,K]) , 2*torch.rand([1 , K])-1
+
+        t_train, t_test = T * torch.rand([1, K]), T * torch.rand([1, K])
+
+        optimizer = optim.AdamW(model.parameters(), lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=1e-9, amsgrad=True)  # Algorithm AdamW
+        best_model, best_loss_train, best_loss_test = model, np.infty, np.infty  # Selects the best minimizer of the Loss function
+        Loss_train = [] # list for loss_train values
+        Loss_test = []  # List for loss_test values
+
+        for epoch in range(N_epochs + 1):
+            for ixs in torch.split(torch.arange(t_train.shape[1]), BS):
+                optimizer.zero_grad()
+                model.train()
+                t_batch = t_train[:, ixs]
+                loss_train = self.Loss_autograd(t_batch, T, model)[0]
+                loss_train.backward()
+                optimizer.step()  # Optimizer passes to the next epoch for gradient descent
+
+            loss_train, loss_train_ODE, loss_train_FC = self.Loss_autograd(t_train, T, model)
+            loss_test, loss_test_ODE, loss_test_FC = self.Loss_autograd(t_test, T, model)
+
+            if loss_train < best_loss_train:
+                best_loss_train = loss_train
+                best_loss_test = loss_test
+                best_model = copy.deepcopy(model)
+                # best_model = model
+
+            Loss_train.append(loss_train.item())
+            Loss_test.append(loss_test.item())
+
+            if epoch % N_epochs_print == 0:  # Print of Loss values (one print each N_epochs_print epochs)
+                end_time_train = start_time_train + ((N_epochs + 1) / (epoch + 1)) * (time.time() - start_time_train)
+                end_time_train = datetime.datetime.fromtimestamp(int(end_time_train)).strftime(' %Y-%m-%d %H:%M:%S')
+                print('    Epoch', epoch, ': Loss_train =', format(loss_train, '.4E'), ': Loss_test =', format(loss_test, '.4E'), " -  Estimated end:", end_time_train)
+                print('       > Train: ODE:', format(loss_train_ODE, '.2E'), '- FC:', format(loss_train_FC, '.2E'))
+                print('       > Test: ODE:', format(loss_test_ODE, '.2E'), '- FC:', format(loss_test_FC, '.2E'))
+
+        print("Loss_train (final)=", format(best_loss_train, '.4E'))
+        print("Loss_test (final)=", format(best_loss_test, '.4E'))
+
+        print("Computation time for training (h:min:s):", str(datetime.timedelta(seconds=int(time.time() - start_time_train))))
+
+        torch.save((Loss_train, Loss_test, best_model , T) , "model_Control_ODE")
+
+        pass
+
+    def Integrate(self, name_model="model_Control_ODE",  ht = 0.02, save_fig=False):
+        """Integrates the PDE with trained model.
+        Inputs:
+        - name_model: Str - Name of the trained model. Default: model_Heat_Equation_1D
+        - ht: Float - Step size for time. Default: 0.02
+        - save_fig: Boolean - Saves the figure or not. Default: False"""
+
+        Loss_train, Loss_test, model, T = torch.load(name_model)
+
+        t = torch.arange(0, T + ht, ht).unsqueeze(0)
+
+        # Resolution with the PINN
+        print(" ")
+        print("Resolution with PINN...")
+        y_PINN, u_PINN = model(t)
+        y_PINN = y_PINN.detach().numpy()
+        u_PINN = u_PINN.detach().numpy()
+
+        plt.figure(figsize=(15,5))
+
+        plt.subplot(1, 3, 1)
+        plt.plot(y_PINN[0, :], y_PINN[1, :], color="black", label=r"$y_{\theta}$")
+        plt.scatter([self.y0()[0, 0], 1.001*self.y0()[0, 0]], [self.y0()[0, 1], 1.001*self.y0()[0, 1]], color="red", label="$y_0$")
+        plt.scatter([self.y1()[0, 0], 1.001*self.y1()[0, 0]], [self.y1()[0, 1], 1.001*self.y1()[0, 1]], color="green", label="$y_1$")
+        ax = plt.gca()
+        plt.axis("equal")
+        plt.grid()
+        plt.legend()
+        ax.set_xlabel("$y_1$")
+        ax.set_ylabel("$y_2$")
+        plt.title(r"$y_{\theta}(t)$ (PINN)")
+
+        plt.subplot(1, 3, 2)
+        plt.plot(t.squeeze(), u_PINN.squeeze(), color="black", label=r"$u_{\theta}$")
+        ax = plt.gca()
+        plt.grid()
+        plt.legend()
+        ax.set_xlabel("t")
+        plt.title(r"$u_{\theta}(t)$ (PINN)")
+
+        plt.subplot(1, 3, 3)
+        plt.plot(list(range(len(Loss_train))), Loss_train, label="$Loss_{train}$", color="green")
+        plt.plot(list(range(len(Loss_test))), Loss_test, label="$Loss_{test}$", color="red")
+        plt.yscale("log")
+        plt.xlabel("epochs")
+        plt.legend()
+        plt.title("Loss evolution")
+        plt.grid()
+
+
+        if save_fig == False:
+            plt.show()
+        else:
+            plt.savefig("PINN_ODE_Control.pdf" , dpi = (500))
+        pass
